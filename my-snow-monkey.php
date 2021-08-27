8dda8861b0c3cea068a5e0636ad4506b2bee45f0
KENTA <qqqyya134@gmail.com>
2021-08-28 01:39:40 +0900

    a

--- my-snow-monkey.php
+++ my-snow-monkey.php
@@ -30,20 +30,10 @@ define( 'MY_SNOW_MONKEY_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
  * @var string
  */
 define( 'MY_SNOW_MONKEY_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
-
-//ローディング画面アニメーション
-add_action(
-	'snow_monkey_prepend_body',
-	function() {
-		?>
-		<div class="c-page-effect" data-page-effect="fadein" aria-hidden="false">
-			<div class="c-page-effect__item">
-				<div class="c-circle-spinner"></div>
-			</div>
-		</div>
-		<?php
-	}
-);
+/**
+ * @param array $items パンくずの配列
+ * @return array パンくずの配列
+ */
 
 // 実際のページ用の CSS 読み込み
 add_action(
@@ -55,19 +45,39 @@ add_action(
 			[ Framework\Helper::get_main_style_handle() ],
 			filemtime( plugin_dir_path( __FILE__ ) )
 		);
+		wp_enqueue_style(
+			'font-awesome',
+			'https://use.fontawesome.com/releases/v5.6.1/css/all.css',
+		);
 	}
 );
-/**
- * FontAwesome ショートコード
- */
-function hsm_add_fontawesome_icon( $atts ) {
-	extract( shortcode_atts( array(
-			'icon' => 'fab fa-wordpress',
-	), $atts ) );
-	$output = '<i class="' . $icon . '"></i>';
-	return $output;
+// js
+add_action(
+	'wp_enqueue_scripts',
+	function() {
+		wp_enqueue_script(
+			'effect-fade',
+			'/wp-content/plugins/my-snow-monkey/js/script.js',
+			[],
+			false
+		);
+	}
+);
+//固定ページにカテゴリー、タグを追加
+add_action('init','add_categories_for_pages'); 
+function add_categories_for_pages(){ 
+   register_taxonomy_for_object_type('category', 'page'); 
+} 
+add_action( 'pre_get_posts', 'nobita_merge_page_categories_at_category_archive' ); 
+function nobita_merge_page_categories_at_category_archive( $query ) { 
+ 
+if ( $query->is_category== true && $query->is_main_query() ) { 
+$query->set('post_type', array( 'post', 'page', 'nav_menu_item')); 
+} 
+} 
+//svgデータを有効か
+function custom_mime_types( $mimes ) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
 }
-add_shortcode( 'hsm-fa', 'hsm_add_fontawesome_icon' );
-// カスタム投稿タイプ(呼び出し)
-add_action('init', 'create_post_type');
-
+add_filter( 'upload_mimes', 'custom_mime_types' );
